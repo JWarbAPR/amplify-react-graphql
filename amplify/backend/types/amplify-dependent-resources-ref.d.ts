001459a61380d384f9f04d340c29ffffc20641d9
JWarbAPR <106659138+JWarbAPR@users.noreply.github.com>
2023-02-02 10:20:47 +0000

    including graph ql

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqlf8ec6336": {
       "AppClientID": "string",
